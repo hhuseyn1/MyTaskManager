da70639c2f38366500947ce6d35f6135513173be
Huseyn Hamidov <88426345+hhuseyn1@users.noreply.github.com>
2023-02-14 18:46:34 +0400

    Add timer load/close

--- MyTaskManager/MainWindow.xaml.cs
+++ MyTaskManager/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using MVVM.Commands;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Linq;
+using System.Timers;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace MyTaskManager;
 
@@ -11,11 +14,67 @@ public partial class MainWindow : Window
 {
    public ObservableCollection<Process> Processes { get; set; }
    public List<string> BlackList { get; set; }
+
+    public ICommand BLCommand { get; set; }
     public MainWindow()
     {
         InitializeComponent();
         DataContext= this;
         Processes = new(Process.GetProcesses());
+        BLCommand = new RelayCommand(BLcommand);
+        var timer = new System.Timers.Timer();
+        timer.Interval = 5000;
+
+        timer.Elapsed += TimerCloseBLProcesses;
+        timer.Elapsed += TimerLoadProcess;
+        timer.Start();
+    }
+
+    private void TimerCloseBLProcesses(object? sender, ElapsedEventArgs e)
+    {
+        Dispatcher.Invoke(() =>
+        {
+            foreach (var process in Processes)
+            {
+                if (BlackList.Any(p => p == process.ProcessName))
+                {
+                    try
+                    {
+                        process.Kill();
+                        Processes.Remove(process);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+            }
+        }
+        );
+    }
+
+    private void TimerLoadProcess(object? sender, ElapsedEventArgs e)
+    {
+        Dispatcher.Invoke(() =>
+        {
+            Tasks.ItemsSource = null;
+            foreach (var process in Process.GetProcesses())
+            {
+                Processes.Add(process);
+            }
+        });
+    }
+
+    private void BLcommand(object? obj)
+    {
+        if (Tasks.SelectedItem is null)
+            return;
+
+        if (Tasks.SelectedItem is Process process)
+        {
+            if (!BlackList.Contains(process.ProcessName))
+                BlackList.Add(process.ProcessName);
+        }
     }
 
     private void Btn_AddBlist(object sender, RoutedEventArgs e)
@@ -23,6 +82,7 @@ public partial class MainWindow : Window
         if (string.IsNullOrWhiteSpace(BlackList_txtbox.Text) || string.IsNullOrEmpty(BlackList_txtbox.Text))
             return;
         BlackList.Add(BlackList_txtbox.Text);
+        BlackList_txtbox.Text = string.Empty;
     }
 
     private void Btn_RunProcess(object sender, RoutedEventArgs e)
@@ -33,6 +93,7 @@ public partial class MainWindow : Window
         try
         {
             Process.Start(RunProcess_txtbox.Text);
+            
         }
         catch (Exception ex)
         {
@@ -51,6 +112,7 @@ public partial class MainWindow : Window
             try
             {
                 process.Kill();
+                Processes.Remove(process);
             }
             catch (Exception ex)
             {
